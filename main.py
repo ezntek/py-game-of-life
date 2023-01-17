ffb8a759a43b6df54c02867eac037f84de32b9bf
Pearl <95740760+DaringCuteSeal@users.noreply.github.com>
2023-01-17 16:15:57 +0700

    more rewrite

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ lifeChart = images.create_image("""
 . . . . .
 """)
 
-state = [ False for i in range(1, 25)]
+#state = [ False for i in range(1, 25)]
+state = [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, ]
 
 def getState(arr: list[bool], x: int, y: int) -> bool:
     return arr[x * 5 + y]
@@ -35,21 +36,35 @@ def show():
     lifeChart.plot_image(0)
 
 def gameOfLife():
-    count: int = 0
+    count = 0
 
     for x in range(1, 5):
         for y in range(1, 5):
             count = 0
 
             if((x + 1) < 5):
-                if(getState(state, x+1, y)):
-                    count = count + 1
+                if getState(state, x+1, y):
+                    count += 1
 
-                if((y + 1) < 5) and getState(state, x+1, y+1):
-                    count = count + 1
+                if (y + 1) < 5 and getState(state, x+1, y+1):
+                    count += 1
                 
-                if((y - 1) >= 5) and getState(state, x+1, y+1)):
-                    count = count + 1
+                if (y - 1) >= 5 and getState(state, x+1, y+1):
+                    count += 1
+            
+            if((x - 1) >= 0):
+                if getState(state, x - 1, y):
+                    count += 1
+                
+                if (y + 1 < 5) and getState(state, x - 1, y + 1):
+                    count += 1
+                
+                if (y - 1 >= 0) and getState(state, x - 1, y - 1):
+                    count += 1
+
+            if 
+
+                
 
 
 
--- main.ts
+++ main.ts
@@ -1,3 +1,76 @@
-basic.forever(function on_forever() {
-    basic
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    gameOfLife()
 })
+let lifeChart = images.createImage(`
+. . . . .
+. . . . .
+. . . . .
+. . . . .
+. . . . .
+`)
+// state = [ False for i in range(1, 25)]
+let state = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+function getState(arr: any, x: number, y: number): boolean {
+    return arr[x * 5 + y]
+}
+
+function setState(arr: any, x: number, y: number, value: boolean) {
+    arr[x * 5 + y] = value
+}
+
+function reset() {
+    for (let x = 1; x < 5; x++) {
+        for (let y = 1; y < 5; y++) {
+            setState(state, x, y, Math.randomBoolean())
+        }
+    }
+}
+
+function show() {
+    for (let x = 1; x < 5; x++) {
+        for (let y = 1; y < 5; y++) {
+            lifeChart.setPixel(x, y, getState(state, x, y))
+        }
+    }
+    lifeChart.plotImage(0)
+}
+
+function gameOfLife() {
+    let count = 0
+    for (let x = 1; x < 5; x++) {
+        for (let y = 1; y < 5; y++) {
+            count = 0
+            if (x + 1 < 5) {
+                if (getState(state, x + 1, y)) {
+                    count += 1
+                }
+                
+                if (y + 1 < 5 && getState(state, x + 1, y + 1)) {
+                    count += 1
+                }
+                
+                if (y - 1 >= 5 && getState(state, x + 1, y + 1)) {
+                    count += 1
+                }
+                
+            }
+            
+            if (x - 1 >= 0) {
+                if (getState(state, x - 1, y)) {
+                    count += 1
+                }
+                
+                if (y + 1 < 5 && getState(state, x - 1, y + 1)) {
+                    count += 1
+                }
+                
+                if (y - 1 >= 0 && getState(state, x - 1, y - 1)) {
+                    count += 1
+                }
+                
+            }
+            
+        }
+    }
+}
+
